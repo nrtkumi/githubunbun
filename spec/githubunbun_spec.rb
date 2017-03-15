b0bda26c7dbfd52af88c70cf84ddd678eb3942e0
TakumiNara <takuminara0428@gmail.com>
2017-03-15 14:25:03 +0900

    TODO clear

--- spec/githubunbun_spec.rb
+++ spec/githubunbun_spec.rb
@@ -4,8 +4,4 @@ describe Githubunbun do
   it 'has a version number' do
     expect(Githubunbun::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
